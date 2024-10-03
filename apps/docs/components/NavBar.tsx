78f7ed4494b7a64969acd9ce9f650c60c17f557a
Sunny <swarnendughosh05@gmail.com>
2024-10-04 00:58:02 +0530

    navbar init

--- apps/docs/components/NavBar.tsx
+++ apps/docs/components/NavBar.tsx
@@ -1,15 +1,24 @@
+import { Button } from '@repo/ui/button'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='bg-transparent  backdrop-blur-md mx-32 py-3 mt-5 rounded-2xl border-s-transparent border-slate-500'>
-      <nav>
-         <div className='font-bold font-mono ml-5'>
-            _AlgoPrep
-         </div>
+    <div className='bg-transparent backdrop-blur-md mx-32 py-3 mt-5 rounded-2xl border border-s-transparent border-slate-700'>
+      <nav className='flex justify-between items-center'>
+
+        <div className='font-mono ml-8 font-normal text-neutral-100'>
+          _AlgoPrep
+        </div>
+        
+        <div className='hidden md:flex space-x-8 mr-5'>
+          <a href='#' className='text-white hover:text-gray-300'>Features</a>
+          <a href='#' className='text-white hover:text-gray-300'>Pricing</a>
+          <div>
+         
+          </div>
+        </div>
       </nav>
     </div>
-
   )
 }
 
