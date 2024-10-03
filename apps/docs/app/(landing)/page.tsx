0eebd9920d15aacd14e96f4c8019e4a398acaee2
Sunny <swarnendughosh05@gmail.com>
2024-10-03 23:04:38 +0530

    bavbar init

--- apps/docs/app/(landing)/page.tsx
+++ apps/docs/app/(landing)/page.tsx
@@ -1,7 +1,10 @@
+import NavBar from "../../components/NavBar";
+
+
 export default function Home() {
   return (
-    <div className="bg-slate-800 h-screen font-extrabold">
-      hi there tets
-    </div>
+    <div>
+       <NavBar />
+   </div>
   );
 }
---
+++ apps/docs/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='bg-transparent  backdrop-blur-md mx-32 py-3 mt-5 rounded-2xl border-s-transparent border-slate-500'>
+      <nav>
+         <div className='font-bold font-mono ml-5'>
+            _AlgoPrep
+         </div>
+      </nav>
+    </div>
+
+  )
+}
+
+export default NavBar
