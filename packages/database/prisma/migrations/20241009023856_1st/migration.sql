d25fbe6392a4f3c2990b25f95720779dc6dc65f1
Sunny <swarnendughosh05@gmail.com>
2024-10-09 08:12:23 +0530

    db added

---
+++ packages/database/prisma/migrations/20241009023856_1st/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
