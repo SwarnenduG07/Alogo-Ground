1de565837a40eff3a4799e4a2193b3b71ca0bfe9
Sunny <swarnendughosh05@gmail.com>
2024-10-09 11:50:49 +0530

    auth chekk added

--- apps/web/lib/auth.action.ts
+++ apps/web/lib/auth.action.ts
@@ -1,5 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import bcrypt from "bcrypt";
+import { dbCLient } from "@/src/app/db";
 export const authOptions = {
     providers: [
         CredentialsProvider({
@@ -15,6 +16,24 @@ export const authOptions = {
                 type: "password",
                 placeholder: "********",
               },
+
+              async authorize(credentials: any) {
+                   if (!credentials.username || !credentials.password) {
+                          return null;
+                   }
+                   const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                   const user = await dbCLient.user.upsert({
+                    where: {
+                      email: credentials.username,
+                    },
+                    update: {},
+                    create: {
+                      email: credentials.username,
+                      name: credentials.username,
+                      password: credentials.password, 
+                     };
+                   });
+                }
             },
         })
     ]
