620c95b191b78b79301adb76d64f5a30fac74847
Sunny <swarnendughosh05@gmail.com>
2024-10-18 20:26:45 +0530

    contestcard added

--- apps/web/components/ContestCard.tsx
+++ apps/web/components/ContestCard.tsx
@@ -1,6 +1,8 @@
-
+"use client"
 import React from 'react'
-import { Card } from './ui/card';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { start } from 'repl';
+import { Button } from './ui/button';
 
 interface Contestcaedparams {
     title: string,
@@ -15,7 +17,43 @@ export function ContestCard  ({ title, id, endTime, startTime}: Contestcaedparam
     const isActive = startTime.getTime() < Date.now() && endTime.getTime() > Date.now();
   return (
     <Card>
-        
+        <CardHeader>
+          <div className='flex justify-between'>
+            <CardTitle>{title}</CardTitle>
+          <div>
+          {startTime.getTime() < Date.now() && endTime.getTime() < Date.now() ? (
+            <div className='text-red-500'>Ended</div>
+          ): null}
+          {isActive ? <div className='text-green-500'>Active</div>: null}
+          {endTime.getTime() < Date.now()  ? (
+            <div className='text-red-500'>Ended</div>
+          ): null}
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className='flwx items-center justify-between'>
+              <div>
+                <p className='text-gray-500'>
+                  {startTime.getTime() < Date.now() ? "Started" : "Starts In"}
+                </p>
+                <p>
+                  {/* {startTime.getTime() < Date.now() ? } */}
+                </p>
+              </div>
+              <div>
+                <p className='text-gray-500'>
+                  Duraction
+                </p>
+                <p>{duraction}</p>
+              </div>
+          </div>
+          <CardFooter>
+            <Button>
+                {isActive ? "Participate" : "View Contest"}
+            </Button>
+          </CardFooter>
+        </CardContent>
     </Card>
   )
 }
