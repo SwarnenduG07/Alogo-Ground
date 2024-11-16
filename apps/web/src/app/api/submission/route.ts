e3ce9490a90077ecacd2d5a3c3617e68e26fbb3a
Sunny <swarnendughosh05@gmail.com>
2024-11-16 18:56:05 +0530

    submission get added

--- apps/web/src/app/api/submission/route.ts
+++ apps/web/src/app/api/submission/route.ts
@@ -131,3 +131,56 @@ export async function POST(req:NextRequest) {
     }
    )
 }
+export async function  GET(req: NextRequest) {
+   const session = await getServerSession(authOptions);
+   if(!session?.user) {
+     return NextResponse.json(
+      {
+        message: "You must be ogged in to view submissions"
+      },
+      {
+        status: 401
+      }
+     );
+   }
+   const url = new URL(req.url);
+   const searchParams = new URLSearchParams(url.search);
+   const submissionId = searchParams.get("id");
+
+   if(!submissionId) {
+     return NextResponse.json(
+      {
+        message: "invalid submission id",
+      },
+       {
+        status: 400,
+       }
+     );
+   }
+  let submission = await dbCLient.submission.findUnique({
+    where: {
+      id: submissionId,
+    },
+    include: {
+      testcases: true,
+    },
+  });
+  if(!submission) {
+    return NextResponse.json(
+      {
+        message: "Submission not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+  return NextResponse.json(
+    {
+      submission,
+    },
+    {
+      status: 200,
+    }
+  );
+}
